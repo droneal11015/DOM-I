fb61e07126ea6137d391c4644a38f4946b892752
droneal11015 <droneal11015@gmail.com>
2019-04-15 18:22:31 -0400

    completed file

--- js/index.js
+++ js/index.js
@@ -90,3 +90,25 @@ let visionH4 = document.getElementsByTagName('h4')[4];
 visionH4.innerHTML = "Vision";
 let visionContent = document.getElementsByTagName('p')[4];
 visionContent.innerHTML = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+//Contact info
+
+let contactH4 = document.getElementsByTagName('h4')[5];
+contactH4.innerHTML = "Contact";
+let address = document.getElementsByTagName('p')[5];
+address.innerHTML = "123 Way 456 Street Somewhere, USA";
+let phone = document.getElementsByTagName('p')[6];
+phone.innerHTML = "1 (888) 888-8888";
+let email = document.getElementsByTagName('p')[7];
+email.innerHTML = "sales@greatidea.io";
+
+//footer
+let footer = document.getElementsByTagName('p')[8];
+footer.innerHTML = "Copyright Great Idea! 2018"
+
+//green nav links
+let navigation = document.querySelectorAll("a");
+let i;
+for (i = 0; i < navigation.length; i++) {
+  navigation[i].style.color = "green";
+}
