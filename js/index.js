4b029bec08561c31f5e5be3b039a40af7e29beef
droneal11015 <droneal11015@gmail.com>
2019-04-15 15:41:29 -0400

    completed code for nav items

--- js/index.js
+++ js/index.js
@@ -46,3 +46,17 @@ headerImg.setAttribute("src", "img/header-img.png")
 
 let middleImg = document.getElementById("middle-img");
 middleImg.setAttribute("src", "img/mid-page-accent.jpg")
+
+//Nav
+let services = document.getElementsByTagName("a")[0];
+services.innerHTML = "Services";
+let product = document.getElementsByTagName("a")[1];
+product.innerHTML = "Product";
+let vision = document.getElementsByTagName("a")[2];
+vision.innerHTML = "Vision";
+let features = document.getElementsByTagName("a")[3];
+features.innerHTML = "Features";
+let about = document.getElementsByTagName("a")[4];
+about.innerHTML = "About"
+let contact = document.getElementsByTagName("a")[5];
+contact.innerHTML = "Contact";
